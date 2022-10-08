54f4ced9a761a448806e07d1bff843e377e293af
marcusPrado02 <silvamarcusprado@gmail.com>
2022-10-08 14:04:03 -0300

    Update sorting service - ordenacao balanceada

--- src/services/SortingService.java
+++ src/services/SortingService.java
@@ -2,7 +2,11 @@ package services;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Scanner;
 
 /*
  *  a) Intercalação balanceada comum
@@ -18,6 +22,7 @@ public class SortingService{
     private int M; // Quantidades de registros a serem ordenados por segmento
     private int N; // Número de caminhos/arquivos temporários
     private HashMap<Integer,Registro> registros;
+    private Scanner sc;
 
     public class Registro {
         public int id;
@@ -32,15 +37,28 @@ public class SortingService{
 
     }
 
+    class RegistroComparator implements Comparator<Registro> {
+        
+        public int compare(Registro r1, Registro r2){
+            if (r1.id == r2.id) {
+                return 0;
+            }
+            else if (r1.id < r2.id) {
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        }
+    }
+
     public SortingService() {
         this.M = 0;
         this.N = 0;
         this.registros = new HashMap<Integer,Registro>();
+        this.sc = new Scanner(System.in);
     }
 
-    public byte[] intercalar(){
-
-    }
 
     public void readFromFile(int opcao) {
         
@@ -48,6 +66,8 @@ public class SortingService{
 
         long pos0 = 0;
         long skipPos0 = 0; long address = 0;
+
+        int counter = 0;
         
         try  {
                 RandomAccessFile arq = new RandomAccessFile("contas.db", "rw");
@@ -79,9 +99,9 @@ public class SortingService{
                     }
 
                     if( opcao == 1){
-                        intercalacaoBalanceada();
+                        intercalacaoBalanceada(counter);
                     }
-                    
+
                     if( opcao == 2){
                         intercalacaoBalanceadaTamanhoVariavel();
                     }
@@ -98,9 +118,8 @@ public class SortingService{
                         intercalacaoPolifasica();
                     }
 
-
-                
-
+                    counter++;
+            
                 }
                 
                 arq.close();
@@ -112,19 +131,84 @@ public class SortingService{
         }
     }
 
-    private void intercalacaoBalanceadaSelecaoPorSubstituicao() {
-    }
-
+    
     public void ListarOpcoes() {
         System.out.println("Opcoes");
+        int opcao = sc.nextInt();
+        readFromFile(opcao);
     }
+    
+    public void intercalacaoBalanceada(int counter){
+        Collection<Registro> tmp = registros.values();
 
-    public void intercalacaoBalanceada(){
+        ArrayList<Registro> segmento = new ArrayList<>();
+        segmento.addAll(tmp);
 
+        RegistroComparator rc = new RegistroComparator();
+        segmento.sort(rc);
+
+        if(counter % 2 == 0){
+            byte[] ba;
+
+            try  {
+                RandomAccessFile arq = new RandomAccessFile("contas.db", "rw");
+                int aux = arq.readInt();
+
+                RandomAccessFile tmpFile = new RandomAccessFile("tmp1.db", "rw");
+                
+
+                for (Registro reg : segmento) {
+                    long addr = reg.address;
+                    int tam = reg.len;
+
+                    arq.seek(addr);
+                    ba = new byte[tam];
+                    arq.read(ba);
+
+                    tmpFile.write(ba);
+                }
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                //System.out.println(e);
+            }
+
+        } else {
+            byte[] ba;
+
+            try  {
+                RandomAccessFile arq = new RandomAccessFile("contas.db", "rw");
+                int aux = arq.readInt();
+
+                RandomAccessFile tmpFile = new RandomAccessFile("tmp2.db", "rw");
+                
+
+                for (Registro reg : segmento) {
+                    long addr = reg.address;
+                    int tam = reg.len;
+
+                    arq.seek(addr);
+                    ba = new byte[tam];
+                    arq.read(ba);
+
+                    tmpFile.write(ba);
+                }
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                //System.out.println(e);
+            }
+
+        }
+        
     }
-
+    
     public void intercalacaoBalanceadaTamanhoVariavel(){
         
+    }
+
+    private void intercalacaoBalanceadaSelecaoPorSubstituicao() {
+
     }
 
     public void intercalacaoBalanceadaNmaisUm(){
