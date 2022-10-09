a8c0f75a76e7859887474a5584326bce8731b822
marcusPrado02 <silvamarcusprado@gmail.com>
2022-10-09 19:39:56 -0300

    Update Sorting

--- src/services/SortingService.java
+++ src/services/SortingService.java
@@ -1,5 +1,6 @@
 package services;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
@@ -17,11 +18,12 @@ import java.util.Scanner;
 
  * */
 
-public class SortingService{
+public class SortingService {
 
-    private int M; // Quantidades de registros a serem ordenados por segmento
-    private int N; // Número de caminhos/arquivos temporários
+    private int tamBloco; // Quantidades de registros a serem ordenados por segmento
+    private int caminhos; // Número de caminhos/arquivos temporários
     private HashMap<Integer,Registro> registros;
+    ArrayList<RandomAccessFile> arrArquivosTmp;
     private Scanner sc;
 
     public class Registro {
@@ -52,10 +54,55 @@ public class SortingService{
         }
     }
 
+    public class RegistroTemporario {
+        public RegistroTemporario(int idBloco2, int tam2, int id2, byte[] ba) {
+        }
+        public int idBloco;
+        public int id;
+        public int tam;
+        public byte[] content;
+
+    }
+
+    class RegistroTemporarioComparator implements Comparator<RegistroTemporario> {
+        
+        public int compare(RegistroTemporario r1, RegistroTemporario r2){
+            if (r1.id == r2.id) {
+                return 0;
+            }
+            else if (r1.id < r2.id) {
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        }
+    }
+    
+
+    public class BlocoDeRegistroTemporario{
+        public ArrayList<RegistroTemporario> regsTmp;
+
+        BlocoDeRegistroTemporario(){
+            regsTmp = new ArrayList<RegistroTemporario>();
+        }
+
+        public RegistroTemporario getMin() {
+
+            RegistroTemporarioComparator rtc = new RegistroTemporarioComparator();
+            regsTmp.sort(rtc);
+            RegistroTemporario result = regsTmp.get(0);
+
+            return result;
+
+        }
+    }
+
     public SortingService() {
-        this.M = 0;
-        this.N = 0;
+        this.tamBloco = 0;
+        this.caminhos = 0;
         this.registros = new HashMap<Integer,Registro>();
+        this.arrArquivosTmp = new ArrayList<RandomAccessFile>();
         this.sc = new Scanner(System.in);
     }
 
@@ -67,7 +114,7 @@ public class SortingService{
         long pos0 = 0;
         long skipPos0 = 0; long address = 0;
 
-        int counter = 0;
+        int numSegmento = 1;
         
         try  {
                 RandomAccessFile arq = new RandomAccessFile("contas.db", "rw");
@@ -82,7 +129,7 @@ public class SortingService{
                 
                 while (pos0 != arq.length()) {
 
-                    for(int i = 0; i < M; i++){
+                    for(int i = 1; i <= tamBloco; i++){
 
                         address = arq.getFilePointer();
                         int tam = arq.readInt();
@@ -90,7 +137,7 @@ public class SortingService{
                         int id = arq.readInt();
                         
                         Registro tmp = new Registro(id, tam, address);
-                        this.registros.put(M,tmp);
+                        this.registros.put(i*numSegmento,tmp);
 
                         
                         skipPos0 = skipPtr.skipBytes(tam);
@@ -98,27 +145,20 @@ public class SortingService{
                     
                     }
 
-                    if( opcao == 1){
-                        intercalacaoBalanceada(counter);
-                    }
+                    if( opcao == 1 || opcao == 2 || opcao == 4){
+                        distribuicaoInicial(numSegmento);
 
-                    if( opcao == 2){
-                        intercalacaoBalanceadaTamanhoVariavel();
                     }
 
                     if( opcao == 3){
                         intercalacaoBalanceadaSelecaoPorSubstituicao();
                     }
 
-                    if( opcao == 4){
-                        intercalacaoBalanceadaNmaisUm();
-                    }
-
                     if( opcao == 5){
                         intercalacaoPolifasica();
                     }
 
-                    counter++;
+                    numSegmento++;
             
                 }
                 
@@ -129,6 +169,14 @@ public class SortingService{
                 // TODO Auto-generated catch block
                 //System.out.println(e);
         }
+
+        /* Aqui comecaremos a chamar os metodos de ordenacao de cada um */
+
+        if(opcao == 1 ){
+            intercalacaoBalanceada(numSegmento);
+        }
+
+
     }
 
     
@@ -138,7 +186,8 @@ public class SortingService{
         readFromFile(opcao);
     }
     
-    public void intercalacaoBalanceada(int counter){
+    public void distribuicaoInicial(int numSegmento){
+
         Collection<Registro> tmp = registros.values();
 
         ArrayList<Registro> segmento = new ArrayList<>();
@@ -147,15 +196,24 @@ public class SortingService{
         RegistroComparator rc = new RegistroComparator();
         segmento.sort(rc);
 
-        if(counter % 2 == 0){
-            byte[] ba;
+        byte[] ba;
 
-            try  {
+        for(int i = 0; i < this.caminhos; i++){
+            String fileName = new String("tempfile"+i+".db");
+            RandomAccessFile arqTemp;
+
+            try {
+                arqTemp = new RandomAccessFile(fileName, "rw");
+                this.arrArquivosTmp.add(arqTemp);
+
                 RandomAccessFile arq = new RandomAccessFile("contas.db", "rw");
                 int aux = arq.readInt();
 
-                RandomAccessFile tmpFile = new RandomAccessFile("tmp1.db", "rw");
-                
+                RandomAccessFile tmpFileOne = arrArquivosTmp.get(i);
+                tmpFileOne.seek(tmpFileOne.length());   
+
+                tmpFileOne.write(numSegmento);
+            
 
                 for (Registro reg : segmento) {
                     long addr = reg.address;
@@ -165,44 +223,118 @@ public class SortingService{
                     ba = new byte[tam];
                     arq.read(ba);
 
-                    tmpFile.write(ba);
+                    tmpFileOne.writeInt(tam);
+                    tmpFileOne.writeInt(reg.id);
+                    tmpFileOne.write(ba);
                 }
+
+                tmpFileOne.close();
+                arq.close();
+
+
 
             } catch (IOException e) {
                 // TODO Auto-generated catch block
-                //System.out.println(e);
+                //e.printStackTrace();
             }
 
-        } else {
-            byte[] ba;
+        }
 
-            try  {
-                RandomAccessFile arq = new RandomAccessFile("contas.db", "rw");
-                int aux = arq.readInt();
+    
 
-                RandomAccessFile tmpFile = new RandomAccessFile("tmp2.db", "rw");
-                
 
-                for (Registro reg : segmento) {
-                    long addr = reg.address;
-                    int tam = reg.len;
 
-                    arq.seek(addr);
-                    ba = new byte[tam];
-                    arq.read(ba);
+        
 
-                    tmpFile.write(ba);
-                }
+        
 
-            } catch (IOException e) {
+        
+    }
+
+    public void intercalacaoBalanceada(int numSegmento){
+
+        ArrayList<RandomAccessFile> acessos = new ArrayList<>();
+        BlocoDeRegistroTemporario blocoRegs = new BlocoDeRegistroTemporario();
+
+        byte[] ba;
+
+        for(int i = 1; i <= caminhos; i++){
+
+            String fileName = new String("tempfileinteracalacao"+i+".db");
+            RandomAccessFile arqTemp;
+
+            try {
+                arqTemp = new RandomAccessFile(fileName, "rw");
+                acessos.add(arqTemp);
+
+            } catch (FileNotFoundException e) {
                 // TODO Auto-generated catch block
-                //System.out.println(e);
+                //e.printStackTrace();
             }
 
         }
         
+        try {
+
+            
+            int idBloco = 1;
+            
+            for(int i = 1; i <=  caminhos; i++){
+
+
+
+                for(int j = 0; j < tamBloco; j++ ){
+                    
+                    //Esse foreach vai ler de um em um registro
+                    //nos N arquivos
+                    for (RandomAccessFile a : arrArquivosTmp) {
+                        idBloco = a.readInt();
+                        int tam = a.readInt();
+                        int id = a.readInt();
+
+                        ba = new byte[tam];
+                        a.read(ba);
+
+                        RegistroTemporario tmp = new RegistroTemporario(idBloco, tam, id, ba);
+                        blocoRegs.regsTmp.add(tmp);
+
+                    }
+
+
+                    // Pega o registro com menor id que foi lido
+                    // no bloco e escrever no proximo arquivo temporario
+                    RegistroTemporario minRegsTmp = blocoRegs.getMin(); 
+                    escreverRegistroTemporario(acessos.get(i), minRegsTmp);
+
+                    
+                    // Quando chegar nos ultimos registro de cada bloco
+                    // ele vai intercalar os que sobraram
+                    if (j == tamBloco - 1){
+                        RegistroTemporarioComparator  rtc = new RegistroTemporarioComparator();
+                        blocoRegs.regsTmp.sort(rtc);
+
+                        for (RegistroTemporario a : blocoRegs.regsTmp) {
+                            escreverRegistroTemporario(acessos.get(i), a);
+                        }
+                    }
+                }
+            }
+
+            
+
+
+
+            
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
-    
+
     public void intercalacaoBalanceadaTamanhoVariavel(){
         
     }
@@ -217,6 +349,17 @@ public class SortingService{
 
     public void intercalacaoPolifasica(){
         
+    }
+
+    public void escreverRegistroTemporario(RandomAccessFile raf, RegistroTemporario rt){
+        try {
+            raf.writeInt(rt.tam);
+            raf.writeInt(rt.id);
+            raf.write(rt.content);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            //e.printStackTrace();
+        }
     }
    
 }
