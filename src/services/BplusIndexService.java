3939b0e434096ee9f2a54ad8aa1a737c4a014ead
marcusPrado02 <silvamarcusprado@gmail.com>
2022-10-10 17:30:06 -0300

    Update B plus Index Service

--- src/services/BplusIndexService.java
+++ src/services/BplusIndexService.java
@@ -193,7 +193,7 @@ public class BplusIndexService {
                         
                         while(key1 <= p.keys1.get(aux)) {
                                 if(key1 == p.keys1.get(aux)){
-                                        ls.add(p.keys2.get(aux))
+                                        ls.add(p.keys2.get(aux));
                                 }
                                 aux++;
 
@@ -255,7 +255,7 @@ public class BplusIndexService {
                         
                         }
 
-                        if(aux == p.n || key1 <= p.keys1.get(i)){
+                        if(aux == p.n || key1 <= p.keys1.get(aux)){
                                 return read(key1, p.childrenPtrs.get(aux));
                         
                         } else {
@@ -274,7 +274,7 @@ public class BplusIndexService {
         public boolean create(int c1, int c2) throws IOException {
 
                 if(c1 < 0  || c2 < 0) {
-                        System.out.println( "Chaves não podem ser negativas" );
+                        System.out.println( "keys .get(ão podem ser negativas" );
                         return false;
                 }
 
@@ -309,6 +309,188 @@ public class BplusIndexService {
                 }
                     
                 return works;
+        }
+
+        private boolean create(long page) throws IOException {
+                
+                if(page == -1) {
+                        cresceu = true;
+                        pageAux = -1;
+                        return false;
+                }
+
+                arq.seek(page);
+                Page p = new Page(order);
+                byte[] bf = new byte[p.tamPage];
+                arq.read(bf);
+                p.byteToPage(bf);
+
+
+                int i = 0;
+                while(i < p.n && (key1Aux > p.keys1.get(i) || (key1Aux == p.keys1.get(i) && key2Aux > p.keys2.get(i)))) {
+                        i++;
+                }
+
+
+                if(i < p.n && p.childrenPtrs.get(0) == -1 && key1Aux == p.keys1.get(i) && key2Aux == p.keys2.get(i)) {
+                        cresceu = false;
+                        return false;
+                }
+
+
+                boolean inserido;
+
+                if(i == p.n || key1Aux < p.keys1.get(i) || (key1Aux == p.keys1.get(i) && key2Aux < p.keys2.get(i))){
+
+                        inserido = create(p.childrenPtrs.get(i));
+                }else{
+
+                        inserido = create(p.childrenPtrs.get(i+1));
+                }
+
+
+                if(!cresceu){
+                        return inserido;
+                }
+
+
+                if(p.n < maxElements) {
+
+                                
+                        for(int j = p.n; j > i ; j--) {
+                                p.keys1.add(j,  p.keys1.get(j-1));
+                                p.keys2.add(j,  p.keys2.get(j-1));
+                                p.childrenPtrs.add(j+1,  p.childrenPtrs.get(j));
+                        }
+                        
+                        
+                        p.keys1.add(i, key1Aux);
+                        p.keys2.add(i, key2Aux);
+                        p.childrenPtrs.add(i+1, pageAux);
+                        p.n++;
+                        
+                        
+                        arq.seek(page);
+                        arq.write(p.pageToByte());
+                        
+                        
+                        cresceu = false;
+                        return true;
+                }
+
+                Page newp = new Page(order);
+        
+                
+                int m = maxElements/2;
+
+                for(int j=0; j < (maxElements - m ); j++) {    
+                        
+                        
+                        newp.keys1.add(j , p.keys1.get(j+m));
+                        newp.keys2.add(j , p.keys2.get(j+m));   
+                        newp.childrenPtrs.add(j+1,  p.childrenPtrs.get(j+m+1));  
+                        
+                        
+                        p.keys1.add(j+m,  0);
+                        p.keys2.add(j+m,  0);
+                        p.childrenPtrs.add(j+m+1,  (long) -1);
+                }
+
+                newp.childrenPtrs.add(0, p.childrenPtrs.get(m));
+                newp.n = maxElements-m;
+                p.n = m;
+
+
+                
+                if(i <= m) {   
+                
+                        
+                        for(int j=m; j>0 && j>i; j--) {
+                                p.keys1.add(j, p.keys1.get(j-1));
+                                p.keys2.add(j, p.keys2.get(j-1));
+                                p.childrenPtrs.add(j+1, p.childrenPtrs.get(j));
+                        }
+                        
+                        
+                        p.keys1.add(i, key1Aux);
+                        p.keys2.add(i, key2Aux);
+                        p.childrenPtrs.add(i+1, pageAux);
+                        p.n++;
+                        
+                        
+                        if(p.childrenPtrs.get(0) == -1) {
+                                key1Aux = newp.keys1.get(0);
+                                key2Aux = newp.keys2.get(0);
+
+                        }else {
+                                key1Aux = p.keys1.get(p.n-1);
+                                key2Aux = p.keys2.get(p.n-1);
+                                p.keys1.add(p.n-1 , 0);
+                                p.keys2.add(p.n-1 , 0);
+                                p.childrenPtrs.add(p.n , -1);
+                                p.n--;
+                        }
+
+                } else {
+
+                        int j;
+
+                        for(j = maxElements - m; j > 0 && (key1Aux < newp.keys1.get(j-1) || (key1Aux == newp.keys1.get(j-1) && key2Aux < newp.keys2.get(j-1) ); j--) {
+                                
+                                newp.keys1.add(j, newp.keys1.get(j-1));
+                                newp.keys2.add(j, newp.keys2.get(j-1));
+                                newp.childrenPtrs.add(j+1, newp.childrenPtrs.get(j));
+                        
+                        }
+
+                        newp.keys1.add(j, key1Aux);
+                        newp.keys2.add(j, key2Aux);
+                        newp.childrenPtrs.add(j+1, pageAux);
+                        newp.n++;
+        
+                        
+                        key1Aux = newp.keys1.get(0);
+                        key2Aux = newp.keys2.get(0);
+                        
+                        
+                        if(p.childrenPtrs.get(0) != -1) {
+
+                                for(j = 0 ; j < newp.n-1 ; j++) {
+                                
+                                        newp.keys1.add(j, newp.keys1.get(j+1));
+                                        newp.keys2.add(j, newp.keys2.get(j+1));
+                                        newp.childrenPtrs.add(j, newp.childrenPtrs.get(j+1));
+                                
+                                }
+                                
+                                newp.childrenPtrs.add(j, newp.childrenPtrs.(j+1));
+                                
+                                
+                                newp.keys1.add(j, 0);
+                                newp.keys2.add(j, 0);
+                                newp.childrenPtrs.add(j+1, (long) -1);
+                                newp.n--;
+                        }
+        
+                }
+                
+                // Se a página era uma folha e apontava para outra folha, 
+                // então atualiza os ponteiros dessa página e da página nova
+                if(p.childrenPtrs[0]==-1) {
+                        newp.proxima=p.proxima;
+                        p.proxima = arquivo.length();
+                }
+        
+                // Grava as páginas no arquivos arquivo
+                pageAux = arquivo.length();
+                arquivo.seek(pageAux);
+                arquivo.write(newp.getBytes());
+        
+                arquivo.seek(pagina);
+                arquivo.write(p.getBytes());
+                
+                return true;
+                }
         }
         
 
