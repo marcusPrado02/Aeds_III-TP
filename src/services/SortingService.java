5fa9dd134df61a8be0e0810d95b80bdb763de2fe
marcusPrado02 <silvamarcusprado@gmail.com>
2022-10-16 17:13:16 -0300

    Sorting first version

--- src/services/SortingService.java
+++ src/services/SortingService.java
@@ -9,21 +9,14 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Scanner;
 
-/*
- *  a) Intercalação balanceada comum
-    b) Intercalação balanceada com blocos de tamanho variável
-    c) Intercalação balanceada com seleção por substituição
-    d) Intercalação balanceada usando n+1 arquivos
-    e) Intercalação Polifásica
 
- * */
 
 public class SortingService {
 
-    private int tamBloco; // Quantidades de registros a serem ordenados por segmento
-    private int caminhos; // Número de caminhos/arquivos temporários
+    private int tamBloco; 
+    private int numBlocos; 
     private HashMap<Integer,Registro> registros;
-    ArrayList<RandomAccessFile> arrArquivosTmp;
+    private ArrayList<TempRegistro> registrosAIntercalar;
     private Scanner sc;
 
     public class Registro {
@@ -54,19 +47,21 @@ public class SortingService {
         }
     }
 
-    public class RegistroTemporario {
-        public RegistroTemporario(int idBloco2, int tam2, int id2, byte[] ba) {
-        }
-        public int idBloco;
+
+    class TempRegistro {
         public int id;
         public int tam;
-        public byte[] content;
-
+        public byte[] regBa;
+        public TempRegistro(int id, int tam, byte[] regBa) {
+            this.id = id;
+            this.tam = tam;
+            this.regBa = regBa;
+        }
     }
 
-    class RegistroTemporarioComparator implements Comparator<RegistroTemporario> {
+    class TempRegistroComparator implements Comparator<TempRegistro> {
         
-        public int compare(RegistroTemporario r1, RegistroTemporario r2){
+        public int compare(TempRegistro r1, TempRegistro r2){
             if (r1.id == r2.id) {
                 return 0;
             }
@@ -80,34 +75,16 @@ public class SortingService {
     }
     
 
-    public class BlocoDeRegistroTemporario{
-        public ArrayList<RegistroTemporario> regsTmp;
-
-        BlocoDeRegistroTemporario(){
-            regsTmp = new ArrayList<RegistroTemporario>();
-        }
-
-        public RegistroTemporario getMin() {
-
-            RegistroTemporarioComparator rtc = new RegistroTemporarioComparator();
-            regsTmp.sort(rtc);
-            RegistroTemporario result = regsTmp.get(0);
-
-            return result;
-
-        }
-    }
-
     public SortingService() {
         this.tamBloco = 0;
-        this.caminhos = 0;
+        this.numBlocos = 0;
         this.registros = new HashMap<Integer,Registro>();
-        this.arrArquivosTmp = new ArrayList<RandomAccessFile>();
+        this.registrosAIntercalar = new ArrayList<TempRegistro>();
         this.sc = new Scanner(System.in);
     }
 
 
-    public void readFromFile(int opcao) {
+    public void readFromFile() {
         
         byte[] result;
 
@@ -145,20 +122,10 @@ public class SortingService {
                     
                     }
 
-                    if( opcao == 1 || opcao == 2 || opcao == 4){
-                        distribuicaoInicial(numSegmento);
+                    
+                    distribuicaoInicial(numSegmento);
 
-                    }
 
-                    if( opcao == 3){
-                        intercalacaoBalanceadaSelecaoPorSubstituicao();
-                    }
-
-                    if( opcao == 5){
-                        intercalacaoPolifasica();
-                    }
-
-                    numSegmento++;
             
                 }
                 
@@ -170,21 +137,15 @@ public class SortingService {
                 //System.out.println(e);
         }
 
-        /* Aqui comecaremos a chamar os metodos de ordenacao de cada um */
-
-        if(opcao == 1 ){
-            intercalacaoBalanceada(numSegmento);
-        }
+        
+        intercalacaoBalanceada(numSegmento);
+        
 
 
     }
 
     
-    public void ListarOpcoes() {
-        System.out.println("Opcoes");
-        int opcao = sc.nextInt();
-        readFromFile(opcao);
-    }
+
     
     public void distribuicaoInicial(int numSegmento){
 
@@ -198,23 +159,21 @@ public class SortingService {
 
         byte[] ba;
 
-        for(int i = 0; i < this.caminhos; i++){
-            String fileName = new String("tempfile"+i+".db");
-            RandomAccessFile arqTemp;
+        
+    
+        try {
+        
+            RandomAccessFile  tmpFile01 = new RandomAccessFile("tmpFile01.db", "rw");
+            RandomAccessFile  tmpFile02 = new RandomAccessFile("tmpFile02.db", "rw"); 
 
-            try {
-                arqTemp = new RandomAccessFile(fileName, "rw");
-                this.arrArquivosTmp.add(arqTemp);
+            RandomAccessFile arq = new RandomAccessFile("contas.db", "rw");
+            int aux = arq.readInt();
 
-                RandomAccessFile arq = new RandomAccessFile("contas.db", "rw");
-                int aux = arq.readInt();
+            tmpFile01.seek(tmpFile01.length());   
+            tmpFile02.seek(tmpFile02.length());
 
-                RandomAccessFile tmpFileOne = arrArquivosTmp.get(i);
-                tmpFileOne.seek(tmpFileOne.length());   
-
-                tmpFileOne.write(numSegmento);
-            
-
+            if(numSegmento % 2 != 0){
+                tmpFile01.write(numSegmento);
                 for (Registro reg : segmento) {
                     long addr = reg.address;
                     int tam = reg.len;
@@ -223,144 +182,339 @@ public class SortingService {
                     ba = new byte[tam];
                     arq.read(ba);
 
-                    tmpFileOne.writeInt(tam);
-                    tmpFileOne.writeInt(reg.id);
-                    tmpFileOne.write(ba);
+                    tmpFile01.writeInt(tam);
+                    tmpFile01.writeInt(reg.id);
+                    tmpFile01.write(ba);
                 }
 
-                tmpFileOne.close();
-                arq.close();
+                segmento.clear();
 
-
-
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                //e.printStackTrace();
             }
 
+            if(numSegmento % 2 == 0){
+                tmpFile02.write(numSegmento);
+                for (Registro reg : segmento) {
+                    long addr = reg.address;
+                    int tam = reg.len;
+
+                    arq.seek(addr);
+                    ba = new byte[tam];
+                    arq.read(ba);
+
+                    tmpFile02.writeInt(tam);
+                    tmpFile02.writeInt(reg.id);
+                    tmpFile02.write(ba);
+                }
+
+                segmento.clear();
+                numBlocos += 1;
+
+            }
+        
+
+
+            tmpFile01.close();
+            tmpFile02.close();
+            arq.close();
+
+
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            //e.printStackTrace();
         }
 
-    
-
-
-
-        
-
-        
 
         
     }
 
     public void intercalacaoBalanceada(int numSegmento){
 
-        ArrayList<RandomAccessFile> acessos = new ArrayList<>();
-        BlocoDeRegistroTemporario blocoRegs = new BlocoDeRegistroTemporario();
+        
+        byte[] ba01;
+        byte[] ba02;
 
-        byte[] ba;
+        int controle = 0;
 
-        for(int i = 1; i <= caminhos; i++){
+        try{
 
-            String fileName = new String("tempfileinteracalacao"+i+".db");
-            RandomAccessFile arqTemp;
+            RandomAccessFile  tmpFile01 = new RandomAccessFile("tmpFile01.db", "rw");
+            RandomAccessFile  tmpFile02 = new RandomAccessFile("tmpFile02.db", "rw"); 
 
-            try {
-                arqTemp = new RandomAccessFile(fileName, "rw");
-                acessos.add(arqTemp);
+            RandomAccessFile  tmpFile03 = new RandomAccessFile("tmpFile03.db", "rw");
+            RandomAccessFile  tmpFile04 = new RandomAccessFile("tmpFile04.db", "rw"); 
 
-            } catch (FileNotFoundException e) {
-                // TODO Auto-generated catch block
-                //e.printStackTrace();
+            RandomAccessFile  sortedFile = new RandomAccessFile("sortedFile.db", "rw"); 
+
+            tmpFile01.seek(0);
+            tmpFile02.seek(0);
+
+            tmpFile03.seek(tmpFile03.length());
+            tmpFile04.seek(tmpFile04.length());
+
+            for(int j = 0; j < numSegmento; j++){
+
+                if(controle % 2 == 0){
+
+                    for(int i = 0 ; i < tamBloco; i++){
+                        mergeBlocks(tmpFile01, tmpFile02, tmpFile03);
+                    }
+                    for (TempRegistro tr : registrosAIntercalar) {
+                        tmpFile03.writeInt(tr.tam);
+                        tmpFile03.writeInt(tr.id);
+                        tmpFile03.write(tr.regBa);
+                        
+                    }
+                    registrosAIntercalar.clear();
+
+                    controle += 1;
+
+
+                } else {
+                    
+                    for(int i = 0 ; i < tamBloco; i++){
+                        mergeBlocks(tmpFile01, tmpFile02, tmpFile04);
+                    }
+                    for (TempRegistro tr : registrosAIntercalar) {
+                        tmpFile03.writeInt(tr.tam);
+                        tmpFile03.writeInt(tr.id);
+                        tmpFile03.write(tr.regBa);
+                        
+                    }
+                    registrosAIntercalar.clear();
+
+                    controle += 1;
+                
+                
+                }
             }
 
-        }
-        
-        try {
+            numSegmento = (numSegmento/2);
 
-            
-            int idBloco = 1;
-            
-            for(int i = 1; i <=  caminhos; i++){
+            for(int i = 1; i < 1000000 && (numSegmento/2 < 1); i++){
+                if(i % 2 != 0){
+                    repetirIntercalacao( numSegmento ,tmpFile03, tmpFile04, tmpFile01, tmpFile02);
+                    numSegmento = (numSegmento/2);
 
-
-
-                for(int j = 0; j < tamBloco; j++ ){
-                    
-                    //Esse foreach vai ler de um em um registro
-                    //nos N arquivos
-                    for (RandomAccessFile a : arrArquivosTmp) {
-                        idBloco = a.readInt();
-                        int tam = a.readInt();
-                        int id = a.readInt();
-
-                        ba = new byte[tam];
-                        a.read(ba);
-
-                        RegistroTemporario tmp = new RegistroTemporario(idBloco, tam, id, ba);
-                        blocoRegs.regsTmp.add(tmp);
-
+                    for (int j = 0; j <= tmpFile03.length(); j++) {
+                        tmpFile03.writeUTF("");    
                     }
 
+                    for (int j = 0; j <= tmpFile04.length(); j++) {
+                        tmpFile04.writeUTF("");    
+                    }
 
-                    // Pega o registro com menor id que foi lido
-                    // no bloco e escrever no proximo arquivo temporario
-                    RegistroTemporario minRegsTmp = blocoRegs.getMin(); 
-                    escreverRegistroTemporario(acessos.get(i), minRegsTmp);
+                } else {
+                    repetirIntercalacao( numSegmento ,tmpFile01, tmpFile02, tmpFile03, tmpFile04);
+                    numSegmento = (numSegmento/2);
 
-                    
-                    // Quando chegar nos ultimos registro de cada bloco
-                    // ele vai intercalar os que sobraram
-                    if (j == tamBloco - 1){
-                        RegistroTemporarioComparator  rtc = new RegistroTemporarioComparator();
-                        blocoRegs.regsTmp.sort(rtc);
+                    for (int j = 0; j <= tmpFile01.length(); j++) {
+                        tmpFile01.writeUTF("");    
+                    }
 
-                        for (RegistroTemporario a : blocoRegs.regsTmp) {
-                            escreverRegistroTemporario(acessos.get(i), a);
-                        }
+                    for (int j = 0; j <= tmpFile02.length(); j++) {
+                        tmpFile02.writeUTF("");    
                     }
                 }
             }
 
+            /*
+             * Ultima passada para voltar pro arquivo original
+             */
+            if(tmpFile01.length() > 1 && tmpFile02.length() > 1 && tmpFile03.length() < 1 && tmpFile04.length() < 1){
+                
+                for(int i = 0 ; i < tamBloco; i++){
+                    mergeBlocks(tmpFile01, tmpFile02, sortedFile);
+                }
+                for (TempRegistro tr : registrosAIntercalar) {
+                    sortedFile.writeInt(tr.tam);
+                    sortedFile.writeInt(tr.id);
+                    sortedFile.write(tr.regBa);
+                    
+                }
+                registrosAIntercalar.clear();
+
+                for (int j = 0; j <= tmpFile01.length(); j++) {
+                    tmpFile01.writeUTF("");    
+                }
+
+                for (int j = 0; j <= tmpFile02.length(); j++) {
+                    tmpFile02.writeUTF("");    
+                }
+
+            }
+
+            if(tmpFile03.length() > 1 && tmpFile04.length() > 1 && tmpFile01.length() < 1 && tmpFile02.length() < 1){
+                for(int i = 0 ; i < tamBloco; i++){
+                    mergeBlocks(tmpFile03, tmpFile04, sortedFile);
+                }
+                for (TempRegistro tr : registrosAIntercalar) {
+                    sortedFile.writeInt(tr.tam);
+                    sortedFile.writeInt(tr.id);
+                    sortedFile.write(tr.regBa);
+                    
+                }
+                registrosAIntercalar.clear();
+
+                for (int j = 0; j <= tmpFile03.length(); j++) {
+                    tmpFile01.writeUTF("");    
+                }
+
+                for (int j = 0; j <= tmpFile04.length(); j++) {
+                    tmpFile02.writeUTF("");    
+                }
+
+            }
+
+
+
+
             
-
-
-
-            
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-    }
-
-    public void intercalacaoBalanceadaTamanhoVariavel(){
-        
-    }
-
-    private void intercalacaoBalanceadaSelecaoPorSubstituicao() {
-
-    }
-
-    public void intercalacaoBalanceadaNmaisUm(){
-        
-    }
-
-    public void intercalacaoPolifasica(){
-        
-    }
-
-    public void escreverRegistroTemporario(RandomAccessFile raf, RegistroTemporario rt){
-        try {
-            raf.writeInt(rt.tam);
-            raf.writeInt(rt.id);
-            raf.write(rt.content);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             //e.printStackTrace();
         }
+
+
     }
+
+    public void repetirIntercalacao(int numSegmento,RandomAccessFile tmpFile01, RandomAccessFile tmpFile02, RandomAccessFile tmpFile03,RandomAccessFile tmpFile04 ) {
+        byte[] ba01;
+        byte[] ba02;
+
+        int controle = 0;
+
+        if(numSegmento/2 < 1){
+            return;
+        }
+
+        try{
+
+            tmpFile01.seek(0);
+            tmpFile02.seek(0);
+
+            tmpFile03.seek(tmpFile03.length());
+            tmpFile04.seek(tmpFile04.length());
+
+            for(int j = 0; j < numSegmento; j++){
+
+                if(controle % 2 == 0){
+
+                    for(int i = 0 ; i < tamBloco; i++){
+                        mergeBlocks(tmpFile01, tmpFile02, tmpFile03);
+                    }
+                    for (TempRegistro tr : registrosAIntercalar) {
+                        tmpFile03.writeInt(tr.tam);
+                        tmpFile03.writeInt(tr.id);
+                        tmpFile03.write(tr.regBa);
+                        
+                    }
+                    registrosAIntercalar.clear();
+
+                    controle += 1;
+
+
+                } else {
+                    
+                    for(int i = 0 ; i < tamBloco; i++){
+                        mergeBlocks(tmpFile01, tmpFile02, tmpFile04);
+                    }
+                    for (TempRegistro tr : registrosAIntercalar) {
+                        tmpFile03.writeInt(tr.tam);
+                        tmpFile03.writeInt(tr.id);
+                        tmpFile03.write(tr.regBa);
+                        
+                    }
+                    registrosAIntercalar.clear();
+
+                    controle += 1;
+                
+                
+                }
+            }
+
+
+            
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            //e.printStackTrace();
+        }
+        
+    }
+
+    public void mergeBlocks(RandomAccessFile tmpFile01, RandomAccessFile tmpFile02, RandomAccessFile tmpFile03){
+
+        byte[] ba01;
+        byte[] ba02;
+
+        int auxNumSegmento;
+        try {
+
+
+            auxNumSegmento = tmpFile01.readInt();
+            auxNumSegmento = tmpFile02.readInt();
+    
+            int tam01 = tmpFile01.readInt();
+            int id01 = tmpFile01.readInt();
+    
+            int tam02 = tmpFile02.readInt();
+            int id02 = tmpFile02.readInt();
+    
+            ba01 = new byte[tam01];
+            tmpFile01.read(ba01);
+    
+            ba02 = new byte[tam02];
+            tmpFile02.read(ba02);
+            
+            if(id01 < id02 && registrosAIntercalar.size() == 0 ){
+                tmpFile03.writeInt(tam01);
+                tmpFile03.writeInt(id01);
+                tmpFile03.write(ba01);
+
+                TempRegistro tmp = new TempRegistro(tam02, id02, ba02);
+                registrosAIntercalar.add(tmp);
+
+                
+            }else if(id01 > id02 && registrosAIntercalar.size() == 0 ){
+                tmpFile03.writeInt(tam02);
+                tmpFile03.writeInt(id02);
+                tmpFile03.write(ba02);
+
+                TempRegistro tmp = new TempRegistro(tam01, id01, ba01);
+                registrosAIntercalar.add(tmp);
+
+                
+            }else if(registrosAIntercalar.size() > 0){
+                registrosAIntercalar.sort(new TempRegistroComparator());
+
+                if(registrosAIntercalar.get(0).id <= id01 && registrosAIntercalar.get(0).id <= id02){
+                    TempRegistro tmp = registrosAIntercalar.get(0);
+                    registrosAIntercalar.remove(0);
+
+                    tmpFile03.writeInt(tmp.tam);
+                    tmpFile03.writeInt(tmp.id);
+                    tmpFile03.write(tmp.regBa);
+
+                    TempRegistro tmp01 = new TempRegistro(tam01, id01, ba01);
+                    registrosAIntercalar.add(tmp01);
+
+                    TempRegistro tmp02 = new TempRegistro(tam02, id02, ba02);
+                    registrosAIntercalar.add(tmp02);
+                }
+
+            }
+
+            
+
+
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            //e.printStackTrace();
+        }
+
+    }
+
    
 }
 
